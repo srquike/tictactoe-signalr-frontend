9b81880c2283b6e929ac0e1e5c37f113fc05201b
srquike <srquike@outlook.com>
2023-09-30 19:57:12 -0600

    Agregar seccion para el componente Game

--- app/join-room/page.tsx
+++ app/join-room/page.tsx
@@ -4,9 +4,11 @@ import JoinPlayerForm from "@/app/_components/JoinPlayerForm/JoinPlayerForm";
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import styles from "./page.module.css";
+import Game from "../_components/Game/Game";
 
 export default function JoinRoom() {
   const [isJoining, setIsJoining] = useState(false);
+  const [isJoined, setIsJoined] = useState(false);
   const params = useSearchParams();
   const roomId = params.get("id");
 
@@ -15,13 +17,19 @@ export default function JoinRoom() {
   }
 
   return (
-    <main className={styles.main}>
-      <h2>Unirse</h2>
-      {isJoining ? (
-        <p>Uniendote, por favor espera...</p>
+    <main>
+      {isJoined ? (
+        <></>
       ) : (
-        <div>
-          <JoinPlayerForm onFormSubmit={handleOnSubmit} />
+        <div className={styles.main}>
+          <h2>Unirse</h2>
+          {isJoining ? (
+            <p>Uniendote, por favor espera...</p>
+          ) : (
+            <div>
+              <JoinPlayerForm onFormSubmit={handleOnSubmit} />
+            </div>
+          )}
         </div>
       )}
     </main>
